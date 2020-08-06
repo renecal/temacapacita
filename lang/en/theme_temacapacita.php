90c17df0ba31ad273beb2a6dfeabf8ece04a7903
René Calfin Olate <rene.calfin@gmail.com>
2020-08-05 22:19:28 -0400

    cargando avance

--- lang/en/theme_temacapacita.php
+++ lang/en/theme_temacapacita.php
@@ -2,11 +2,36 @@
 // Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
  
 // This line protects the file from being accessed by a URL directly.                                                               
-defined('MOODLE_INTERNAL') || die();                                                                                                
- 
-// A description shown in the admin theme selector.                                                                                 
-$string['choosereadme'] = 'Theme temacapacita is a child theme of Boost.';                
-// The name of our plugin.                                                                                                          
-$string['pluginname'] = 'temacapacita';                                                                                                    
-// We need to include a lang string for each block region.                                                                          
+defined('MOODLE_INTERNAL') || die();
+
+$string['advancedsettings'] = 'Advanced settings';
+$string['backgroundimage'] = 'Background image';
+$string['backgroundimage_desc'] = 'The image to display as a background of the site. The background image you upload here will override the background image in your theme preset files.';
+$string['brandcolor'] = 'Brand colour';
+$string['brandcolor_desc'] = 'The accent colour.';
+$string['bootswatch'] = 'Bootswatch';
+$string['bootswatch_desc'] = 'A bootswatch is a set of Bootstrap variables and css to style Bootstrap';
+$string['choosereadme'] = 'Boost is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+$string['currentinparentheses'] = '(current)';
+$string['configtitle'] = 'Template Capacita';
+$string['fontsize'] = 'Theme base fontsize';
+$string['fontsize_desc'] = 'Enter a fontsize in %';
+$string['generalsettings'] = 'General settings';
+$string['nobootswatch'] = 'None';
+$string['pluginname'] = 'Template Capacita';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files, and see the <a href="https://archive.moodle.net/boost">Presets repository</a> for presets that others have shared.';
+$string['preset'] = 'Theme preset';
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['privacy:metadata'] = 'The Boost theme does not store any personal data about any user.';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+$string['rawscsspre'] = 'Raw initial SCSS';
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 $string['region-side-pre'] = 'Right';
+$string['privacy:metadata:preference:draweropennav'] = 'The user\'s preference for hiding or showing the drawer menu navigation.';
+$string['privacy:drawernavclosed'] = 'The current preference for the navigation drawer is closed.';
+$string['privacy:drawernavopen'] = 'The current preference for the navigation drawer is open.';
+$string['totop'] = 'Go to top';
+                                                                                              
+ 
